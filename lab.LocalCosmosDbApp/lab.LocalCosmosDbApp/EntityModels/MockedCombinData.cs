1ecb785bc306e2a7847d0e17239aab728bf4cf42
Rasel Ahmmed Bappi <raselahmmed@hotmail.com>
2021-12-28 15:31:21 +0600

    Create MockedCombinData

---
+++ lab.LocalCosmosDbApp/lab.LocalCosmosDbApp/EntityModels/MockedCombinData.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab.LocalCosmosDbApp.EntityModels
+{
+    public class MockedCombinedData
+    {
+        public string CombinedToolKey { get; set; }
+        public string CommonNameMaster { get; set; }
+        public string FiscalQuarter { get; set; }
+        public string ToolIdMaster { get; set; }
+        public string ToolOrSpaceDetailsLabSpace { get; set; }
+        public string ToolIdAndToolName { get; set; }
+        public string AssetNumber { get; set; }
+        public string DataSourceLabSpace { get; set; }
+        public string DataSource { get; set; }
+        public string CampusLabSpace { get; set; }
+        public string BuildingLabSpace { get; set; }
+        public string SpaceCommonName { get; set; }
+        public string SpaceLabSpace { get; set; }
+        public string ToolBayLabSpace { get; set; }
+        public string BayLabAllocation { get; set; }
+        public string BayToolProfile { get; set; }
+        public string LabToolProfile { get; set; }
+        public string RoomToolProfile { get; set; }
+        public string AssetType { get; set; }
+        public string ToolTypeLabSpace { get; set; }
+        public string ToolTypeToolProfile { get; set; }
+        public string ChamberFacet { get; set; }
+        public string BusinessUnitMaster { get; set; }
+        public string BUToolProfile { get; set; }
+        public string CostCenterToolProfile { get; set; }
+        public string AssetCustodianMaster { get; set; }
+        public string PlatformMaster { get; set; }
+        public string PlatformSPGLab { get; set; }
+        public string PlatformLabAllocation { get; set; }
+        public string ProductMaster { get; set; }
+        public string ProductSPGLab { get; set; }
+        public string ProdChamber { get; set; }
+        public string ApplicationMaster { get; set; }
+        public string ApplicationsSPGLab { get; set; }
+        public string ApplicationChamber { get; set; }
+        public string LabManagerToolProfile { get; set; }
+        public string ToolOwnerToolProfile { get; set; }
+        public string ChamberOwner { get; set; }
+        public string ToolCountSPGLab { get; set; }
+        public string InstalledChamberSPGLab { get; set; }
+        public string PossibleChamberSPGLab  { get; set; }
+    }
+}
