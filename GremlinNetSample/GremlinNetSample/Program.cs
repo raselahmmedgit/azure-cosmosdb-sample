34fad735acd0483bc4354b4631bcda67b2a814a5
Rasel Ahmmed Bappi <raselahmmed@hotmail.com>
2022-01-13 16:25:33 +0600

    2022-01-13

---
+++ GremlinNetSample/GremlinNetSample/Program.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using Gremlin.Net.Driver;
+using Gremlin.Net.Driver.Exceptions;
+using Gremlin.Net.Structure.IO.GraphSON;
+using Newtonsoft.Json;
+
+namespace GremlinNetSample
+{
+    /// <summary>
+    /// Sample program that shows how to get started with the Graph (Gremlin) APIs for Azure Cosmos DB using the open-source connector Gremlin.Net
+    /// </summary>
+    class Program
+    {
+        // Azure Cosmos DB Configuration variables
+        // Replace the values in these variables to your own.
+        // <configureConnectivity>
+        //private static string Host => Environment.GetEnvironmentVariable("Host") ?? throw new ArgumentException("Missing env var: Host");
+        //private static string PrimaryKey => Environment.GetEnvironmentVariable("PrimaryKey") ?? throw new ArgumentException("Missing env var: PrimaryKey");
+        //private static string Database => Environment.GetEnvironmentVariable("DatabaseName") ?? throw new ArgumentException("Missing env var: DatabaseName");
+        //private static string Container => Environment.GetEnvironmentVariable("ContainerName") ?? throw new ArgumentException("Missing env var: ContainerName");
+
+        private static string Host = "placovugremlinapi.gremlin.cosmos.azure.com";
+        private static string PrimaryKey = "6kDqfxJqYqZZq6NZHMj60AKIgg4DZC55o3cKWsmwTL7cFRjwaIyPJhwFXTED94WqzP38jOYAW3Ms56Fy8nMMKA==";
+        private static string Database = "placovu-database";
+        private static string Container = "placovu-graph";
+
+        //private static bool EnableSSL
+        //{
+        //    get
+        //    {
+        //        if (Environment.GetEnvironmentVariable("EnableSSL") == null)
+        //        {
+        //            return true;
+        //        }
+
+        //        if (!bool.TryParse(Environment.GetEnvironmentVariable("EnableSSL"), out bool value))
+        //        {
+        //            throw new ArgumentException("Invalid env var: EnableSSL is not a boolean");
+        //        }
+
+        //        return value;
+        //    }
+        //}
+
+        //private static int Port
+        //{
+        //    get
+        //    {
+        //        if (Environment.GetEnvironmentVariable("Port") == null)
+        //        {
+        //            return 443;
+        //        }
+
+        //        if (!int.TryParse(Environment.GetEnvironmentVariable("Port"), out int port))
+        //        {
+        //            throw new ArgumentException("Invalid env var: Port is not an integer");
+        //        }
+
+        //        return port;
+        //    } 
+        //}
+
+        private static bool EnableSSL
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        private static int Port
+        {
+            get
+            {
+                return 443;
+            }
+        }
+
+        // </configureConnectivity>
+
+        // Gremlin queries that will be executed.
+        // <defineQueries>
+        private static Dictionary<string, string> gremlinQueries = new Dictionary<string, string>
+        {
+            { "Cleanup",        "g.V().drop()" },
+            { "AddVertex 1",    "g.addV('person').property('id', 'rasel').property('firstName', 'Rasel').property('age', 36).property('pk', 'pk')" },
+            { "AddVertex 2",    "g.addV('person').property('id', 'ayaan').property('firstName', 'Ayaan').property('lastName', 'Ahmmed').property('age', 2).property('pk', 'pk')" },
+            { "AddVertex 3",    "g.addV('person').property('id', 'jamal').property('firstName', 'Jamal').property('lastName', 'Mia').property('pk', 'pk')" },
+            { "AddVertex 4",    "g.addV('person').property('id', 'sohel').property('firstName', 'Sohel').property('lastName', 'Ahmmed').property('pk', 'pk')" },
+            { "AddEdge 1",      "g.V('rasel').addE('knows').to(g.V('ayaan'))" },
+            { "AddEdge 2",      "g.V('rasel').addE('knows').to(g.V('jamal'))" },
+            { "AddEdge 3",      "g.V('jamal').addE('knows').to(g.V('sohel'))" },
+            { "UpdateVertex",   "g.V('rasel').property('age', 36)" },
+            { "CountVertices",  "g.V().count()" },
+            { "Filter Range",   "g.V().hasLabel('person').has('age', gt(40))" },
+            { "Project",        "g.V().hasLabel('person').values('firstName')" },
+            { "Sort",           "g.V().hasLabel('person').order().by('firstName', decr)" },
+            { "Traverse",       "g.V('rasel').out('knows').hasLabel('person')" },
+            { "Traverse 2x",    "g.V('rasel').out('knows').hasLabel('person').out('knows').hasLabel('person')" },
+            { "Loop",           "g.V('rasel').repeat(out()).until(has('id', 'sohel')).path()" },
+            { "DropEdge",       "g.V('rasel').outE('knows').where(inV().has('id', 'ayaan')).drop()" },
+            { "CountEdges",     "g.E().count()" },
+            { "DropVertex",     "g.V('rasel').drop()" },
+        };
+        // </defineQueries>
+
+        // Starts a console application that executes every Gremlin query in the gremlinQueries dictionary. 
+        static void Main(string[] args)
+        {
+            try
+            {
+                // <defineClientandServerObjects>
+                //string containerLink = "/dbs/" + Database + "/colls/" + Container;
+                //Console.WriteLine($"Connecting to: host: {Host}, port: {Port}, container: {containerLink}, ssl: {EnableSSL}");
+                //var gremlinServer = new GremlinServer(Host, Port, enableSsl: EnableSSL, username: containerLink, password: PrimaryKey);
+
+                string containerLink = "/dbs/" + Database + "/colls/" + Container;
+                Console.WriteLine($"Connecting to: host: {Host}, port: {Port}, container: {containerLink}, ssl: {EnableSSL}");
+                var gremlinServer = new GremlinServer(Host, Port, enableSsl: EnableSSL, username: containerLink, password: PrimaryKey);
+
+                ConnectionPoolSettings connectionPoolSettings = new ConnectionPoolSettings()
+                {
+                    MaxInProcessPerConnection = 10,
+                    PoolSize = 30,
+                    ReconnectionAttempts = 3,
+                    ReconnectionBaseDelay = TimeSpan.FromMilliseconds(500)
+                };
+
+                var webSocketConfiguration =
+                    new Action<ClientWebSocketOptions>(options =>
+                    {
+                        options.KeepAliveInterval = TimeSpan.FromSeconds(10);
+                    });
+
+
+                using (var gremlinClient = new GremlinClient(
+                    gremlinServer,
+                    new GraphSON2Reader(),
+                    new GraphSON2Writer(),
+                    GremlinClient.GraphSON2MimeType,
+                    connectionPoolSettings,
+                    webSocketConfiguration))
+                {
+                    // </defineClientandServerObjects>
+
+                    // <executeQueries>
+                    foreach (var query in gremlinQueries)
+                    {
+                        Console.WriteLine(String.Format("Running this query: {0}: {1}", query.Key, query.Value));
+
+                        // Create async task to execute the Gremlin query.
+                        var resultSet = SubmitRequest(gremlinClient, query).Result;
+                        if (resultSet.Count > 0)
+                        {
+                            Console.WriteLine("\tResult:");
+                            foreach (var result in resultSet)
+                            {
+                                // The vertex results are formed as Dictionaries with a nested dictionary for their properties
+                                string output = JsonConvert.SerializeObject(result);
+                                Console.WriteLine($"\t{output}");
+                            }
+                            Console.WriteLine();
+                        }
+
+                        // Print the status attributes for the result set.
+                        // This includes the following:
+                        //  x-ms-status-code            : This is the sub-status code which is specific to Cosmos DB.
+                        //  x-ms-total-request-charge   : The total request units charged for processing a request.
+                        //  x-ms-total-server-time-ms   : The total time executing processing the request on the server.
+                        PrintStatusAttributes(resultSet.StatusAttributes);
+                        Console.WriteLine();
+                    }
+                    // </executeQueries>
+                }
+
+                // Exit program
+                Console.WriteLine("Done. Press any key to exit...");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                // Exit program
+                Console.WriteLine(ex.InnerException.ToString());
+                Console.ReadLine();
+            }
+            
+        }
+
+        private static Task<ResultSet<dynamic>> SubmitRequest(GremlinClient gremlinClient, KeyValuePair<string, string> query)
+        {
+            try
+            {
+                return gremlinClient.SubmitAsync<dynamic>(query.Value);
+            }
+            catch (ResponseException e)
+            {
+                Console.WriteLine("\tRequest Error!");
+
+                // Print the Gremlin status code.
+                Console.WriteLine($"\tStatusCode: {e.StatusCode}");
+
+                // On error, ResponseException.StatusAttributes will include the common StatusAttributes for successful requests, as well as
+                // additional attributes for retry handling and diagnostics.
+                // These include:
+                //  x-ms-retry-after-ms         : The number of milliseconds to wait to retry the operation after an initial operation was throttled. This will be populated when
+                //                              : attribute 'x-ms-status-code' returns 429.
+                //  x-ms-activity-id            : Represents a unique identifier for the operation. Commonly used for troubleshooting purposes.
+                PrintStatusAttributes(e.StatusAttributes);
+                Console.WriteLine($"\t[\"x-ms-retry-after-ms\"] : { GetValueAsString(e.StatusAttributes, "x-ms-retry-after-ms")}");
+                Console.WriteLine($"\t[\"x-ms-activity-id\"] : { GetValueAsString(e.StatusAttributes, "x-ms-activity-id")}");
+
+                throw;
+            }
+        }
+
+        private static void PrintStatusAttributes(IReadOnlyDictionary<string, object> attributes)
+        {
+            Console.WriteLine($"\tStatusAttributes:");
+            Console.WriteLine($"\t[\"x-ms-status-code\"] : { GetValueAsString(attributes, "x-ms-status-code")}");
+            Console.WriteLine($"\t[\"x-ms-total-server-time-ms\"] : { GetValueAsString(attributes, "x-ms-total-server-time-ms")}");
+            Console.WriteLine($"\t[\"x-ms-total-request-charge\"] : { GetValueAsString(attributes, "x-ms-total-request-charge")}");
+        }
+
+        public static string GetValueAsString(IReadOnlyDictionary<string, object> dictionary, string key)
+        {
+            return JsonConvert.SerializeObject(GetValueOrDefault(dictionary, key));
+        }
+
+        public static object GetValueOrDefault(IReadOnlyDictionary<string, object> dictionary, string key)
+        {
+            if (dictionary.ContainsKey(key))
+            {
+                return dictionary[key];
+            }
+
+            return null;
+        }
+    }
+}
